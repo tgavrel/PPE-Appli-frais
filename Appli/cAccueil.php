387e6428966d4b283e734617194f992bb6e516bc
tgavrel <tomas.gavrel@hotmail.fr>
2018-02-01 15:16:29 +0100

    First_commit

---
+++ Appli/cAccueil.php
@@ -0,0 +1,25 @@
+<?php
+/** 
+ * Page d'accueil de l'application web AppliFrais
+ * @package default
+ * @todo  RAS
+ */
+  $repInclude = './Includes/';
+  require($repInclude . "_init.inc.php");
+
+  // page inaccessible si visiteur non connecté
+  if ( ! estVisiteurConnecte() ) 
+  {
+        header("Location: cSeConnecter.php");  
+  }
+  require($repInclude . "_entete.inc.php");
+  require($repInclude . "_sommaire.inc.php");
+?>
+  <!-- Division principale -->
+  <div id="contenu">
+      <h2>Bienvenue sur l'intranet GSB des visiteurs médicaux</h2>
+  </div>
+<?php        
+  require($repInclude . "_pied.inc.html");
+  require($repInclude . "_fin.inc.php");
+?>
---
+++ Appli/cAccueilComptable.php
@@ -0,0 +1,25 @@
+<?php
+/** 
+ * Page d'accueil de l'application web AppliFrais
+ * @package default
+ * @todo  RAS
+ */
+  $repInclude = './Includes/';
+  require($repInclude . "_init.inc.php");
+
+  // page inaccessible si visiteur non connecté
+  if ( ! estVisiteurConnecte() ) 
+  {
+        header("Location: cSeConnecter.php");  
+  }
+  require($repInclude . "_entete.inc.php");
+  require($repInclude . "_sommaireC.inc.php");
+?>
+  <!-- Division principale -->
+  <div id="contenu">
+      <h2>Bienvenue sur l'intranet GSB des comptables</h2>
+  </div>
+<?php        
+  require($repInclude . "_pied.inc.html");
+  require($repInclude . "_fin.inc.php");
+?>
